55d7240efd8911dc62094a769a67623f0dd5ceee
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2022-03-12 22:26:31 +0000

    Displayed array bars

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -2,7 +2,12 @@ import { useState, useEffect } from "react";
 import "./sorting-visualizer.css";
 
 import { randomIntFromInterval } from "./helpers";
-import { ARRAY_BARS } from "./variables";
+import {
+  ARRAY_BARS,
+  MIN_RANDOM_NUMBER,
+  MAX_RANDOM_NUMBER,
+  PRIMARY_COLOR,
+} from "./variables";
 
 const SortingVisualizer = () => {
   const [array, setArray] = useState([]);
@@ -10,7 +15,7 @@ const SortingVisualizer = () => {
   const generateNewArray = () => {
     const array = [];
     for (let i = 0; i < ARRAY_BARS; i++) {
-      array.push(randomIntFromInterval(3, 999));
+      array.push(randomIntFromInterval(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER));
     }
     setArray(array);
   };
@@ -20,13 +25,25 @@ const SortingVisualizer = () => {
     generateNewArray();
   }, []);
 
-  console.log(array);
-
   return (
-    <div className="content">
-      <h1>Sorting Visualizer</h1>
-      <button onClick={() => generateNewArray()}>Generate New Array</button>
-    </div>
+    <>
+      <header className="header">
+        <h1>Sorting Visualizer</h1>
+        <button onClick={() => generateNewArray()}>Generate New Array</button>
+      </header>
+      <main className="array-container" style={{ height: MAX_RANDOM_NUMBER }}>
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}
+          ></div>
+        ))}
+      </main>
+    </>
   );
 };
 
--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,11 @@
 // Change this value for the number of bars in the array.
 export const ARRAY_BARS = 150;
+
+// Change this value to set the min random number and min height of the array bar.
+export const MIN_RANDOM_NUMBER = 3;
+
+// Change this value to set the max random number and max height of the array bar.
+export const MAX_RANDOM_NUMBER = 600;
+
+// The main color of the array bars.
+export const PRIMARY_COLOR = "indianred";
