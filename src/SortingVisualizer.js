69c7a397b90955c8ad0c54e82071416215df726d
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2022-03-12 21:52:37 +0000

    Functionality to generate a new array with random items

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./sorting-visualizer.css";
 
+import { randomIntFromInterval } from "./helpers";
+import { ARRAY_BARS } from "./variables";
+
 const SortingVisualizer = () => {
-  return <div className="content">Sorting Visualizer</div>;
+  const [array, setArray] = useState([]);
+
+  const generateNewArray = () => {
+    const array = [];
+    for (let i = 0; i < ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(5, 730));
+    }
+    setArray(array);
+  };
+
+  console.log(array);
+
+  return (
+    <div className="content">
+      <h1>Sorting Visualizer</h1>
+      <button onClick={() => generateNewArray()}>Generate New Array</button>
+    </div>
+  );
 };
 
 export default SortingVisualizer;
---
+++ src/variables.js
@@ -0,0 +1,2 @@
+// Change this value for the number of bars in the array.
+export const ARRAY_BARS = 150;
