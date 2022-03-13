7aaaa53205f06956d402829d5380218d2fdb46af
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2022-03-13 01:00:46 +0000

    Styled app

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,5 @@
 // Change this value for the number of bars in the array.
-export const ARRAY_BARS = 150;
+export const ARRAY_BARS = 80;
 
 // Change this value to set the min random number and min height of the array bar.
 export const MIN_RANDOM_NUMBER = 3;
@@ -8,10 +8,10 @@ export const MIN_RANDOM_NUMBER = 3;
 export const MAX_RANDOM_NUMBER = 600;
 
 // The main color of the array bars.
-export const PRIMARY_COLOR = "indianred";
+export const PRIMARY_COLOR = "#1792AD";
 
 // The color of array bars that are being compared throughout the animations.
-export const SECONDARY_COLOR = "yellow";
+export const SECONDARY_COLOR = "#FBFF09";
 
 // Value for the speed of the animations in milliseconds.
-export const ANIMATION_SPEED = 1;
+export const ANIMATION_SPEED = 3;
